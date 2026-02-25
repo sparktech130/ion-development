3c711f41aacbe68df3a4ee4175fb70b674c480a0
sparktech130 <sparktech130@gmail.com>
2026-02-25 20:43:25 +0100

    Delete city/src directory

--- city/src/utils/functions/accessibility.js
+++
@@ -1,7 +0,0 @@
-    
-    //Al pulsar la tecla key ejecuta el onclick
-    export const handleKey = (event, onClick, key='Enter') => {
-        if(event.key === key){
-            onClick() 
-        }
-    }
